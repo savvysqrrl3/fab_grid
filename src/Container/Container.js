a51ac7aee3b3be9a4505c894f2d1540c14e6b871
April Cound <musicwithapril@gmail.com>
2019-01-25 19:29:33 -0800

    refactored components, added alignment css

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import styles from './styles.less';
-// import Product from './Product';
+import Product from './Product';
 
 class Container extends React.Component {
   render() {
-    return <div className={styles.productList}>{/* <Product /> */}</div>;
+    return (
+      <div className={styles.rows}>
+        <div className={styles.individualRow}>
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <div className={styles.individualRow}>
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/Container/Product/Product.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.less';
+
+class Product extends React.Component {
+  render() {
+    return <div className={styles.product}>Product Name, etc.</div>;
+  }
+}
+
+export default Product;
