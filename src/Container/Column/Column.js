24dbfdc7f11404604f38e7fe1820cc879080b529
April Cound <musicwithapril@gmail.com>
2019-01-26 15:25:11 -0800

    added hard-coded data to render Product component by mapping

--- src/Container/Column/Column.js
+++ src/Container/Column/Column.js
@@ -2,6 +2,28 @@ import React from 'react';
 import styles from './styles.less';
 import Product from '../Product';
 // import productList from './mockData';
+let data = [
+  {
+    id: '12345',
+    name: 'item 1'
+  },
+  {
+    id: '67890',
+    name: 'item2'
+  },
+  {
+    id: '67890',
+    name: 'item3'
+  },
+  {
+    id: '13581',
+    name: 'item4'
+  },
+  {
+    id: '53421',
+    name: 'item5'
+  }
+];
 
 class Column extends React.Component {
   //   state = productList;
@@ -9,16 +31,9 @@ class Column extends React.Component {
     // const { products } = this.state;
     return (
       <div className={styles.productList}>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {data.map(product => (
+          <Product key={product.id} />
+        ))}
       </div>
     );
   }
