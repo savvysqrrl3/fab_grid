7a1be9834b440477e8fc47487f842ba4182917e2
April Cound <musicwithapril@gmail.com>
2019-01-26 22:40:06 -0800

    Product component now a Draggable

--- src/Container/Product/Product.js
+++ src/Container/Product/Product.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import styles from './styles.less';
 import PropTypes from 'prop-types';
+import { Draggable } from 'react-beautiful-dnd';
 
 class Product extends React.Component {
   render() {
-    return <div className={styles.product}>{this.props.product.name}</div>;
+    return (
+      <Draggable draggableId={this.props.product.id} index={this.props.index}>
+        {provided => (
+          <div
+            provided={provided}
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            className={styles.product}
+          >
+            {this.props.product.name}
+          </div>
+        )}
+      </Draggable>
+    );
   }
 }
 
 Product.propTypes = {
   product: PropTypes.object,
-  name: PropTypes.string
+  name: PropTypes.string,
+  index: PropTypes.number
 };
 
 export default Product;
